1511e68d695a96153ccc78072fc6c335bcdf0078
lshillman <lshillman@users.noreply.github.com>
2022-08-24 22:29:19 -0700

    add ProjectList component

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import ProjectList from '../ProjectList'
+
+const projects = [
+  {
+    id: 1,
+    title: "Anglo Piano",
+    description: "A fun app build in Axure RP to help anglo concertina players find chords and compose music",
+    image: "anglopiano.jpg",
+    repo: "https://github.com/lshillman/anglo-piano",
+    deployed: "https://github.com/lshillman/anglo-piano"
+  }
+];
 
 export default function Portfolio() {
   return (
+    <>
     <div>
       <h1>Portfolio</h1>
       <p>
@@ -9,5 +22,7 @@ export default function Portfolio() {
       </p>
       <p>[image: the lone and level sands stretch far away]</p>
     </div>
+    <ProjectList projects={projects} />
+    </>
   );
 }
